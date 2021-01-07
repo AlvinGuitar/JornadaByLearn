1b089d03a009d34917b0f8dddf7dce760f72fe81
AlvinGuitar <77109887+AlvinGuitar@users.noreply.github.com>
2021-01-07 11:17:40 -0300

    Add files via upload

---
+++ media.py
@@ -0,0 +1,12 @@
+nome = input('Insira seu nome:')
+
+nota1 = float( input('Sua primmeira nota:'))
+nota2 = float( input('Sua segunda nota:'))
+
+print("Olá", nome, "suas notas foram:")
+print(nota1, "e", nota2)
+
+soma = nota1 + nota2
+media = soma / 2
+
+print("Sua média é", media)
